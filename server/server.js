f483ad217e8989ee41a16826ded22548de9705ac
Nclement <clementndatabaye@gmail.com>
2019-09-02 12:09:23 +0200

    feat(view): add view specific mentor
    add controllers and models
    add helpers
    
    [Finishes #168244520]

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './routes/router';
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.send({ message: 'Welcome to Free Mentors' });
+});
+
+app.use('/api/v1', routes);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+export default app;
