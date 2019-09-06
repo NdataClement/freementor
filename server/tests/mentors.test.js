4812b91b076cd53ad67635863bec337f15641094
Nclement <clementndatabaye@gmail.com>
2019-09-06 17:55:06 +0200

    correct tests

--- server/tests/mentors.test.js
+++ server/tests/mentors.test.js
@@ -34,6 +34,41 @@ describe('Not allowed', () => {
                 expect(res.status).to.be.equal(400);
             });
     });
+    it('check if not mentor', () => {
+
+        chai.request(app)
+
+        .post('/api/v1/auth/signin')
+
+        .send({
+
+            email: "ndataclenhf@gmail.com",
+            password: "$2a$10$g8O5wwQVDhvAi6xkcVDnyuBOOditRjvJtCozf4.Y2R6sQ/EbWmcaO",
+
+        })
+
+        .end((err, res) => {
+
+            expect(res.body.status).to.equal(400);
+
+            expect(res.body).to.have.property('status');
+
+            expect(res.body).to.have.property('error');
+
+            expect(res.body).to.be.an('object');
+
+        });
+
+    });
+    it('wrong id of session', () => {
+        chai.request(app)
+            .get('/api/v1/sessions/2')
+            .end((err, res) => {
+                expect(res.status).to.equal(404);
+
+
+            });
+    })
 
     it('Only mentor can accept session', () => {
         chai.request(app)
